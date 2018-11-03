ebb0d2d1c436db49deaa36481dbc075a9c260426
Ivan Triviño <webmaster@prosumertech.com.co>
2018-11-03 08:03:43 -0500

    First attempt to comunicate front and back

--- src/templates/assets/js/update_uploaded_image.js
+++ src/templates/assets/js/update_uploaded_image.js
@@ -1,11 +1,11 @@
+
 function readURL(input){
 	if(input.files && input.files[0]){
 		var reader = new FileReader();
-
+		
 		reader.onload = function(e){
 			var img_tag = document.getElementById('uploaded-bill-image');
 			img_tag.src = e.target.result
-			// $('#uploaded-bill-image').attr('src', e.target.result);
 		}
 
 		reader.readAsDataURL(input.files[0]);
@@ -14,6 +14,26 @@ function readURL(input){
 
 var img_input = document.getElementById('image');
 img_input.addEventListener('change', (event) => {
-	console.log(event.target);
 	readURL(event.target);
 });
+
+var send_btn = document.getElementById('send_btn');
+	send_btn.addEventListener('click', (event) => {
+		var loading_modal = document.getElementById('loading_modal');
+		loading_modal.classList.add("active");
+		
+		var formData = new FormData();
+		formData.append('file', img_input.files[0]);
+
+		if(formData){
+			$.ajax({
+				url: "http://localhost:5001/receipts",
+				type: "POST",
+				data: formData,
+				processData: false,
+				success: function (msg) {
+	  				console.log(msn);
+				},
+			});
+		}
+	});
